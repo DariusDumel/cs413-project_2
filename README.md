3640ae433e639ebe94d5351a509ab87b7e83270b
Dumilk <dariusdum@gmail.com>
2019-09-24 10:31:28 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# project_2
+ puzzle game for cs413
---
+++ js/game.js
@@ -0,0 +1,7 @@
+const app = new PIXI.Application({
+    width: 500,
+    height: 500,
+    backgroundColor: 0xff69b4,
+});
+
+document.getElementById("gameport").appendChild(app.view);
